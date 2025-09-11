3eeaf3adef557f9eeb0979fc9a2885a154e09139
Jeremy-Verweij <aj.verweij@st.hanze.nl>
2025-09-11 09:04:13 +0200

    removed javaFx dependency

--- src/main/java/com/isy/Main.java
+++ src/main/java/com/isy/Main.java
@@ -1,64 +1,63 @@
 package com.isy;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board();
-        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(board);
-        System.out.println("X: ");
-
-        boolean xOrY = true;
-        int x = 0, y = 0;
-        while (scanner.hasNext()){
-            String next = scanner.next();
-
-            if (next.equalsIgnoreCase("exit()")){
-                scanner.close();
-                break;
-            }else if (xOrY) {
-                try {
-                    x = Integer.parseInt(next);
-                }catch (NumberFormatException e){
-                    System.out.println("Unexpected input: '" + next + "'");
-                    continue;
-                }
-
-                if (x > 3 || x < 1){
-                    System.out.println("Input: '" + next + "' needs to be between 1 and 3");
-                    continue;
-                }
-
-                x--;
-            }else{
-                try {
-                    y = Integer.parseInt(next);
-                }catch (NumberFormatException e){
-                    System.out.println("Unexpected input: '" + next + "'");
-                    continue;
-                }
-
-                if (y > 3 || y < 1){
-                    System.out.println("Input: '" + next + "' needs to be between 1 and 3");
-                    continue;
-                }
-
-                y--;
-            }
-
-            xOrY = !xOrY;
-
-            if (xOrY){
-                board.setTile(x, y, Tile.X);
-
-                System.out.println(board);
-
-                System.out.println("X: ");
-            }else{
-                System.out.println("Y: ");
-            }
-        }
+//        Board board = new Board();
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.println(board);
+//        System.out.println("X: ");
+//
+//        boolean xOrY = true;
+//        int x = 0, y = 0;
+//        while (scanner.hasNext()){
+//            String next = scanner.next();
+//
+//            if (next.equalsIgnoreCase("exit()")){
+//                scanner.close();
+//                break;
+//            }else if (xOrY) {
+//                try {
+//                    x = Integer.parseInt(next);
+//                }catch (NumberFormatException e){
+//                    System.out.println("Unexpected input: '" + next + "'");
+//                    continue;
+//                }
+//
+//                if (x > 3 || x < 1){
+//                    System.out.println("Input: '" + next + "' needs to be between 1 and 3");
+//                    continue;
+//                }
+//
+//                x--;
+//            }else{
+//                try {
+//                    y = Integer.parseInt(next);
+//                }catch (NumberFormatException e){
+//                    System.out.println("Unexpected input: '" + next + "'");
+//                    continue;
+//                }
+//
+//                if (y > 3 || y < 1){
+//                    System.out.println("Input: '" + next + "' needs to be between 1 and 3");
+//                    continue;
+//                }
+//
+//                y--;
+//            }
+//
+//            xOrY = !xOrY;
+//
+//            if (xOrY){
+//                board.setTile(x, y, Tile.X);
+//
+//                System.out.println(board);
+//
+//                System.out.println("X: ");
+//            }else{
+//                System.out.println("Y: ");
+//            }
+//        }
     }
 }
