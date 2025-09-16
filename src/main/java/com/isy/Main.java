0791734e90c73b6056671d5ce2813a33e3e3b1f6
wietse <wietse.h.dejonge@hotmail.com>
2025-09-16 15:25:03 +0200

    replaced old game logic with game logic that uses player object.

--- src/main/java/com/isy/Main.java
+++ src/main/java/com/isy/Main.java
@@ -3,63 +3,38 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Board board = new Board();
+        Scanner scanner = new Scanner(System.in);
 
-//        Board board = new Board();
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println(board);
-//        System.out.println("X: ");
-//
-//        boolean xOrY = true;
-//        int x = 0, y = 0;
-//        while (scanner.hasNext() && !board.checkWin(x, y, null)){
-//            String next = scanner.next();
-//
-//            if (next.equalsIgnoreCase("exit()")){
-//                scanner.close();
-//                break;
-//            }else if (xOrY) {
-//                try {
-//                    x = Integer.parseInt(next);
-//                }catch (NumberFormatException e){
-//                    System.out.println("Unexpected input: '" + next + "'");
-//                    continue;
-//                }
-//
-//                if (x > 3 || x < 1){
-//                    System.out.println("Input: '" + next + "' needs to be between 1 and 3");
-//                    continue;
-//                }
-//
-//                x--;
-//            }else{
-//                try {
-//                    y = Integer.parseInt(next);
-//                }catch (NumberFormatException e){
-//                    System.out.println("Unexpected input: '" + next + "'");
-//                    continue;
-//                }
-//
-//                if (y > 3 || y < 1){
-//                    System.out.println("Input: '" + next + "' needs to be between 1 and 3");
-//                    continue;
-//                }
-//
-//                y--;
-//            }
-//
-//            xOrY = !xOrY;
-//
-//            if (xOrY){
-//                board.setTile(x, y, Tile.X);
-//
-//                System.out.println(board);
-//
-//                System.out.println("X: ");
-//            }else{
-//                System.out.println("Y: ");
-//            }
-//
-//        }
+        // TIJDELIJKE WERKENDE SPELER TEGEN SPELER SIMULATIE
+
+        HumanPlayer speler1 = new HumanPlayer("Jantje", Tile.X, scanner);
+        HumanPlayer speler2 = new HumanPlayer("Pietje", Tile.O, scanner);
+
+        Player currentPlayer = speler1;
+
+        while (true) {
+            System.out.println(board);
+
+            int[] move = currentPlayer.getMove(board);
+            boolean validMove = board.setTile(move[0], move[1], currentPlayer.getSymbol());
+
+            if(!validMove){
+                System.out.println("Dit vakje is al bezet!");
+                continue;
+            }
+
+            if (board.checkWin(move[0], move[1], currentPlayer)){
+                System.out.println(currentPlayer.getName() + " heeft gewonnen!");
+                break;
+            }
+
+            if(currentPlayer == speler1){
+                currentPlayer = speler2;
+            } else {
+                currentPlayer = speler1;
+            }
+        }
+
     }
 }
