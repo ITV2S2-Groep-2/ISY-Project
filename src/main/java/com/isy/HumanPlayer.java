715fcbcae5cf86152779362b1ceba5b3190cefe2
wietse <wietse.h.dejonge@hotmail.com>
2025-09-16 15:27:37 +0200

    little typo

--- src/main/java/com/isy/HumanPlayer.java
+++ src/main/java/com/isy/HumanPlayer.java
@@ -12,7 +12,7 @@ public class HumanPlayer extends Player{
 
     @Override
     public int[] getMove(Board board) {
-        System.out.println(getName() + getSymbol() + " geef het x-coördinaat (1 t/m 3): ");
+        System.out.println(getName() + " (" +  getSymbol() + ")" + " geef het x-coördinaat (1 t/m 3): ");
         int x = scanner.nextInt() - 1;
 
         System.out.println("Geef het y coördinaat (1 t/m 3): ");
