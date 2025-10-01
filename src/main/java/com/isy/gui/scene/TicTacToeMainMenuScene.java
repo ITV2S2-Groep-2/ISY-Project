4aafc1ff14f1d70948774523926fd8c70c31da82
wietse <wietse.h.dejonge@hotmail.com>
2025-10-01 15:14:22 +0200

    Nieuwe basis van selecteren game (twee dropdowns)

--- src/main/java/com/isy/gui/scene/TicTacToeMainMenuScene.java
+++ src/main/java/com/isy/gui/scene/TicTacToeMainMenuScene.java
@@ -7,10 +7,12 @@ import com.isy.gui.Style;
 import com.isy.gui.Window;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.util.UUID;
 
 public class TicTacToeMainMenuScene extends MenuScene{
+    static JComboBox dropdown1, dropdown2;
     private GameServer client;
     private String ownName;
 
@@ -22,30 +24,72 @@ public class TicTacToeMainMenuScene extends MenuScene{
     public void init() {
         JPanel panel = this.getScenePanel();
 
-        panel.add(createHeader("TicTacToe"));
-        panel.add(createDefaultButton("Player VS Player(Offline)", this::startTicTacToeGameAgainstPlayer), getConstraints());
-        panel.add(createDefaultButton("Player VS AI(Offline)", this::startTicTacToeGameAgainstAi), getConstraints());
-        panel.add(createDefaultButton("AI VS AI(Offline)"), getConstraints());
-        panel.add(createDefaultButton("Join game server (temp)", this::goToJoinGameServer), getConstraints());
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(10, 10, 10, 10);
 
-        panel.setBackground(Style.menuBackgroundColor);
+        String options[] = { "Player", "AI", "Remote" };
+
+        dropdown1 = new JComboBox(options);
+        dropdown2 = new JComboBox(options);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        panel.add(dropdown1, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        panel.add(dropdown2, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.gridwidth = 2;
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(createHeader("TicTacToe"), gbc);
+
+        gbc.gridy = 2;
+        gbc.gridwidth = 1;
+        panel.add(createDefaultButton("Start Game", this::startGame), gbc);
+
     }
 
-    private void startTicTacToeGameAgainstPlayer(ActionEvent actionEvent) {
+    private void startGame(ActionEvent e){
+        String player1Type = (String) dropdown1.getSelectedItem();
+        String player2Type = (String) dropdown2.getSelectedItem();
+
+        // Basis opties nu gedaan, zoals player tegen AI, later nog bijv. AI tegen AI afvangen.
+
+        if (player1Type.equals("Player") && player2Type.equals("Player")) {
+            startTicTacToeGameAgainstPlayer();
+        } else if (player1Type.equals("Player") && player2Type.equals("AI") ||
+                player1Type.equals("AI") && player2Type.equals("Player")) {
+            startTicTacToeGameAgainstAi();
+        } else if (player1Type.equals("AI") && player2Type.equals("AI")) {
+//            startAiVsAiGame();
+        } else if (player1Type.equals("Player") || player2Type.equals("Remote")) {
+            goToJoinGameServer();
+        } else {
+            // fallback
+            JOptionPane.showMessageDialog(this.getScenePanel(),
+                    "Ongeldige combinatie gekozen!", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private void startTicTacToeGameAgainstPlayer() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame(new Player[]{new HumanPlayer("1", Tile.X, null), new HumanPlayer("2", Tile.O, null)});
         ticTacToeGame.setRenderScene(this.getWindow().getManager().getScene("ticTacToe"));
         new Thread(ticTacToeGame).start();
         this.getWindow().getManager().showScene("ticTacToe");
     }
 
-    private void startTicTacToeGameAgainstAi(ActionEvent actionEvent) {
+    private void startTicTacToeGameAgainstAi() {
         TicTacToeGame ticTacToeGame = new TicTacToeGame(new Player[]{new HumanPlayer("1", Tile.X, null), new AiPlayer("2", Tile.O, null)});
         ticTacToeGame.setRenderScene(this.getWindow().getManager().getScene("ticTacToe"));
         new Thread(ticTacToeGame).start();
         this.getWindow().getManager().showScene("ticTacToe");
     }
 
-    private void goToJoinGameServer(ActionEvent actionEvent) {
+    private void goToJoinGameServer() {
         client = new GameServer("127.0.0.1", 7789);
         new Thread(client).start();
 
