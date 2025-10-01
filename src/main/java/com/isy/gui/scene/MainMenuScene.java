1cde76cd5f82b08689651855074915a60054f985
wietse <wietse.h.dejonge@hotmail.com>
2025-10-01 13:34:01 +0200

    begin nieuwe scene implementatie, begint nu met keuze othello of tic-tac-toe

---
+++ src/main/java/com/isy/gui/scene/MainMenuScene.java
@@ -0,0 +1,34 @@
+package com.isy.gui.scene;
+
+import com.isy.game.GameServer;
+import com.isy.game.Player;
+import com.isy.game.ticTacToe.*;
+import com.isy.gui.Style;
+import com.isy.gui.Window;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.UUID;
+
+public class MainMenuScene extends MenuScene{
+
+    public MainMenuScene(Window window) {
+        super("MainMenu", window);
+    }
+
+    @Override
+    public void init() {
+        JPanel panel = this.getScenePanel();
+
+        panel.add(createHeader("Selecteer je spel!"));
+        panel.add(createDefaultButton("Tic-tac-toe", this::goToTicTacToeMainMenu), getConstraints());
+        panel.add(createDefaultButton("Othello"), getConstraints());
+        panel.setBackground(Style.menuBackgroundColor);
+    }
+
+    private void goToTicTacToeMainMenu(ActionEvent actionEvent) {
+        this.getWindow().getManager().showScene("ticTacToeMainMenu");
+    }
+
+
+}
