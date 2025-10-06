8aa2bcbdc681749beb3e8da4a331c3da0f3b3f14
wietse <wietse.h.dejonge@hotmail.com>
2025-10-06 10:25:29 +0200

    bool isOnline
     to check where to land go back button on win

--- src/main/java/com/isy/gui/scene/WinScene.java
+++ src/main/java/com/isy/gui/scene/WinScene.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 
 public class WinScene extends MenuScene{
     private JLabel title;
+    private boolean online;
 
     public WinScene(Window window) {
         super("winScene", window);
@@ -19,14 +20,19 @@ public class WinScene extends MenuScene{
         title = createHeader("TicTacToe");
 
         panel.add(title, getConstraints());
-        panel.add(createDefaultButton("Go back to main menu", this::goBackToMainMenu), getConstraints());
+        panel.add(createDefaultButton("Go back", this::goBackToMainMenu), getConstraints());
     }
 
     private void goBackToMainMenu(ActionEvent actionEvent) {
-        this.getWindow().getManager().showScene("ticTacToeMainMenu");
+        if(online){
+            this.getWindow().getManager().showScene("joinGameServerMenuScene");
+        } else {
+            this.getWindow().getManager().showScene("ticTacToeMainMenu");
+        }
     }
 
-    public void win(String playerName){
+    public void win(String playerName, boolean online){
+        this.online = online;
         this.getWindow().getManager().showScene(this.getName());
         title.setText(playerName + " Won!");
     }
