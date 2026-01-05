4d4baa111d3d28be5ff1c7cccaec94fb7e447b51
Jeremy-Verweij <109823690+JeremyVerweij@users.noreply.github.com>
2026-01-05 09:33:54 +0100

    Allow for visible text editing for text fields(WIP, probably has some bugs)

--- src/main/java/com/isy/gui/components/swing/RoundedTextField.java
+++ src/main/java/com/isy/gui/components/swing/RoundedTextField.java
@@ -17,6 +17,7 @@ public class RoundedTextField extends JTextField {
         setOpaque(false);
         setCursor(Cursor.getPredefinedCursor(Cursor.TEXT_CURSOR));
         setPreferredSize(new Dimension(256, 64));
+        setForeground(this.textColor);
     }
 
     @Override
@@ -25,7 +26,13 @@ public class RoundedTextField extends JTextField {
     }
 
     @Override
+    public void repaint(long tm, int x, int y, int width, int height) {
+        super.repaint(tm, 0, 0, getWidth(), getHeight());
+    }
+
+    @Override
     protected void paintComponent(Graphics g) {
+
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
@@ -33,15 +40,15 @@ public class RoundedTextField extends JTextField {
         g2.setColor(backgroundColor);
         g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
 
-        // Tekst
-        g2.setColor(textColor);
         FontMetrics fm = g2.getFontMetrics();
         String text = getText();
-
         int x = (getWidth() - fm.stringWidth(text)) / 2;
-        int y = (getHeight() + fm.getAscent() - fm.getDescent()) / 2;
-        g2.drawString(text, x, y);
 
-        g2.dispose();
+
+        g.translate(x, 0);
+
+        super.paintComponent(g);
+
+        g.translate(-x, 0);
     }
 }
