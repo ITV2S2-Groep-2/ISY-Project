dc05eab50a4c7c67355e544fc768c4556d235c7b
JeremyVerweij <jeremyverweij1234@gmail.com>
2026-01-17 13:47:18 +0100

    Fixed an oversight

--- src/main/java/com/isy/game/GameType.java
+++ src/main/java/com/isy/game/GameType.java
@@ -46,12 +46,12 @@ public enum GameType {
                     default -> null;
                 };
             }
-            case RANDOMAI -> {
-                return switch (gameType) {
-                    case OTHELLO -> OthelloRandomAI.class;
-                    default -> null;
-                };
-            }
+//            case RANDOMAI -> {
+//                return switch (gameType) {
+//                    case OTHELLO -> OthelloRandomAI.class;
+//                    default -> null;
+//                };
+//            }
 
             case REMOTE -> {
                 return switch (gameType) {
